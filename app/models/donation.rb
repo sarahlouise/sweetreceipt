fda6c8dba77d7f9e94ed302e5d2e17031e88040b
Sarah Kruberg <sarah.kruberg@gmail.com>
2015-06-03 01:35:32 -0700

    basic presence validations added for receipts and donation items

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,4 +1,10 @@
 class Donation < ActiveRecord::Base
 	belongs_to :receipt
+	validates :receipt, presence: true
+
 	belongs_to :user
+
+	validates :item_category, presence: true
+	validates :item_type, presence: true
+	validates :value, presence: true
 end
--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -1,4 +1,10 @@
 class Receipt < ActiveRecord::Base
   belongs_to :user
+  validates :user, presence: true
+
   has_many :donations
+
+  validates :title, presence: true
+  validates :title, uniqueness: true
+
 end
